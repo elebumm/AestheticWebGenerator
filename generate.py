4a4516ac3f3c8fbda48c889ff47dedbd9fbd2e4f
Lewis Menelaws <lmenelaws112@gmail.com>
2016-08-21 23:21:00 -0400

    Added new function and added comments
    Added function: generate_background that generates css background

--- generate.py
+++ generate.py
@@ -2,12 +2,16 @@ import seapunknames
 import random
 
 
+# function that grabs a random element from seapunknames.py arrays: 1, 2 and 3 and creates a <title> tag
 def generate_title():
-    rand1 = random.choice(seapunknames.firstArray)
-    rand2 = random.choice(seapunknames.secondArray)
-    rand3 = random.choice(seapunknames.thirdArray)
-    # rand4 = random.choice(seapunknames.fourthArray)
-    return "<title>" + rand3 + rand1 + rand2 + rand3 + "</title>"
+    random_title_1 = random.choice(seapunknames.firstArray)
+    random_title_2 = random.choice(seapunknames.secondArray)
+    random_title_3 = random.choice(seapunknames.thirdArray)
+    return "<title>" + random_title_3 + random_title_1 + random_title_2 + random_title_3 + "</title>"
 
 
+# function that grabs a random element from seapunknames.py array: 4 and creates a background-image: css property
+def generate_background():
+    random_background = random.choice(seapunknames.fourthArray)
+    return "background-image: url('" + random_background + "')"
 
